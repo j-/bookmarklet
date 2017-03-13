3d081e646cc2a33a5c9af014f13d79304ecef268
j- <j@skeoh.com>
2017-03-14 09:35:22 +1100

    Disable bookmarklet click

--- src/components/Bookmarklet.jsx
+++ src/components/Bookmarklet.jsx
@@ -4,10 +4,15 @@ const buildHref = (source) => (
 	`javascript:void(()=>{${source}})()`
 );
 
+const handleClick = (e) => {
+	e.preventDefault();
+};
+
 const Bookmarklet = ({ source, children }) => (
 	<a
 		className="bookmarklet"
 		href={ buildHref(source) }
+		onClick={ handleClick }
 	>
 		{ children }
 	</a>
