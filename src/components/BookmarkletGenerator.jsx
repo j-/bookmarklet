0393ce2a4127c2dd5c11e7768550b89eeed655ab
j- <j@skeoh.com>
2017-03-14 09:47:07 +1100

    Persist state to local storage

--- src/components/BookmarkletGenerator.jsx
+++ src/components/BookmarkletGenerator.jsx
@@ -4,10 +4,12 @@ import Bookmarklet from './Bookmarklet';
 
 import 'codemirror/mode/javascript/javascript';
 
+const LOCAL_STORAGE_KEY = 'bookmarklet-generator-state';
+
 export default class BookmarkletGenerator extends Component {
 	constructor (props) {
 		super(props);
-		this.state = {
+		this.state = this.retrieveState() || {
 			source: '',
 			title: 'Untitled bookmarklet',
 		};
@@ -19,17 +21,35 @@ export default class BookmarkletGenerator extends Component {
 		this.updateTitle = this.updateTitle.bind(this);
 	}
 
+	persistState (state) {
+		try {
+			const serialized = JSON.stringify(state);
+			localStorage.setItem(LOCAL_STORAGE_KEY, serialized);
+		} catch (err) {
+			// Ignore errors
+		}
+	}
+
+	retrieveState () {
+		try {
+			const serialized = localStorage.getItem(LOCAL_STORAGE_KEY);
+			return JSON.parse(serialized);
+		} catch (err) {
+			return null;
+		}
+	}
+
 	updateSource (source) {
 		this.setState({
 			source,
-		});
+		}, () => this.persistState(this.state));
 	}
 
 	updateTitle (e) {
 		const title = e.target.value;
 		this.setState({
 			title,
-		});
+		}, () => this.persistState(this.state));
 	}
 
 	render () {
