1be4602c90b1e79a8889e7f6ba586443afeb0104
j- <j@skeoh.com>
2017-06-20 19:27:37 +1000

    Link to Wikipedia entry

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -7,6 +7,12 @@ const AppHeader = () => (
 		</div>
 		<div className="pt-navbar-group pt-align-right">
 			<a
+				className="pt-button pt-minimal pt-icon-globe"
+				href="https://en.wikipedia.org/wiki/Bookmarklet"
+			>
+				&lsquo;Bookmarklet&rsquo; on Wikipedia
+			</a>
+			<a
 				className="pt-button pt-minimal pt-icon-code"
 				href="https://github.com/j-/bookmarklet"
 			>
