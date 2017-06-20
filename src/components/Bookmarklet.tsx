dd3fe2a17d70f2940c8c95185f2884beb18f433c
j- <j@skeoh.com>
2017-06-20 19:39:49 +1000

    Add click handler to bookmarklet

--- src/components/Bookmarklet.tsx
+++ src/components/Bookmarklet.tsx
@@ -24,10 +24,16 @@ export default class Bookmarklet extends React.Component<Props, void> {
 			<a
 				className="pt-button pt-fill Bookmarklet"
 				href={href}
+				onClick={this.handleClick}
 			>
 				<span className="pt-icon-standard pt-icon-document" />
 				{title || <Untitled />}
 			</a>
 		);
+	}
+
+	private handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+		e.preventDefault();
+		alert('This link must be dragged to your bookmarks');
 	}
 }
