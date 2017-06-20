c7f501f6b84d7251e9a71b36995cb150a1065b42
j- <j@skeoh.com>
2017-06-20 19:13:05 +1000

    Add Blueprint

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,9 +1,19 @@
 import * as React from 'react';
 
 const AppHeader = () => (
-	<div>
-		<h1>Bookmarklet</h1>
-	</div>
+	<nav className="pt-navbar">
+		<div className="pt-navbar-group pt-align-left">
+			<div className="pt-navbar-heading">Bookmarklet</div>
+		</div>
+		<div className="pt-navbar-group pt-align-right">
+			<a
+				className="pt-button pt-minimal pt-icon-code"
+				href="https://github.com/j-/bookmarklet"
+			>
+				Fork me on GitHub
+			</a>
+		</div>
+	</nav>
 );
 
 export default AppHeader;
